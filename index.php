93798fc7db660310d7acb6d99e9a624340231356
ainu <ainu.sky@gmail.com>
2011-05-18 14:21:32 +0400

    test commit

--- index.php
+++ index.php
@@ -14,5 +14,5 @@ $end_time = microtime();
 $end_array = explode(" ",$end_time);
 $end_time = $end_array[1] + $end_array[0];
 $time = $end_time - $start_time;
-//printf("Страница сгенерирована за %f секунд",$time);
+ printf("Страница сгенерирована за %f секунд",$time);
 ?>
