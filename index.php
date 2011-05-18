11c2fdeb0f73245159ea848ed34bfc6979498d62
ainu <ainu.sky@gmail.com>
2011-05-18 14:17:52 +0400

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$start_time = microtime();
+$start_array = explode(" ",$start_time);
+$start_time = $start_array[1] + $start_array[0];
+ 
+include_once ('config.php');
+include_once ('cms/cms.php');
+header('Content-type: text/html; Charset=UTF-8');
+
+print doit()->main();
+
+
+$end_time = microtime();
+$end_array = explode(" ",$end_time);
+$end_time = $end_array[1] + $end_array[0];
+$time = $end_time - $start_time;
+//printf("Страница сгенерирована за %f секунд",$time);
+?>
