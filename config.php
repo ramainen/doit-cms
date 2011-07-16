c3f1c883a433b3c7272db9f5cb0d037ff21ff765
ainu <ainu.sky@gmail.com>
2011-07-16 20:13:54 +0400

    wiki

--- config.php
+++ config.php
@@ -7,6 +7,8 @@ $prefix="vv_";
 
     mysql_connect($hostname,$username,$password);
     mysql_select_db($database);
- 
+ mysql_query ("set character_set_client='utf8'"); 
+mysql_query ("set character_set_results='utf8'"); 
+mysql_query ("set collation_connection='utf8_general_ci'");
     
 ?>
--- tpl/mod_wiki/wiki.func.php
+++ tpl/mod_wiki/wiki.func.php
@@ -2,6 +2,16 @@
 //модель wiki. Содержит функции добавления, вывода отдельной статьи, вспомогательные функции для работы с текстом (Jevix), 
 function wiki_default()
 {
-$asdssd='Вики...';
-	print 'Вики!';
+	global $prefix;
+	//Опрелеоение закголовка страницы
+	$page=  substr(urldecode($_SERVER['REQUEST_URI']),6); //FIXME использовать API получения адресов
+	$page=str_replace('_',' ',$page);
+	
+	//Получение даных из базы данных
+	$result=mysql_query("select * from ".doit()->table." where title LIKE '$page' limit 1");
+	if( $line = mysql_fetch_array($result)) {
+		print doit()->wiki_default_tpl($line);
+	}else{
+		print "Страница не найдена";
+	}
 }
