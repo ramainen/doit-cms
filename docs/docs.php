3a3392e500748907dd2973e22d045dc3f8c567b9
ainu <ainu.sky@gmail.com>
2011-08-28 01:04:37 +0400

    Просмотр актуальной документации по адресу /docs/docs.php

---
+++ docs/docs.php
@@ -0,0 +1,32 @@
+<?php header('Content-type: text/html; Charset=UTF-8');
+include('markdown.php');
+$content =  Markdown(file_get_contents('about.md')); ?><!doctype html>
+<html>
+<head>
+<title>Документация по системе doit cms</title>
+<style>
+html
+{
+	background:#eeeeee;
+		margin-top:0;
+	padding-top:0;
+}
+	body
+	{
+	background:white;
+
+	padding:30px;
+	margin-top:0;
+		width:800px;
+		margin:auto;
+	
+		color: black;
+		font-family: Palatino, "Palatino Linotype", "URW Palladio L", "Book Antiqua", Georgia, serif;
+		line-height: 1.45;
+	}
+</style>
+</head>
+<body>
+	<?=$content ?>
+</body>
+</html>
