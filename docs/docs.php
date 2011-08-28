169c12b20b0bbbac7a839846d9657c2e7269660e
ainu <ainu.sky@gmail.com>
2011-08-28 12:45:41 +0400

    Доработка документации

--- docs/docs.php
+++ docs/docs.php
@@ -1,31 +1,30 @@
-<?php header('Content-type: text/html; Charset=UTF-8');
-include('markdown.php');
-$content =  Markdown(file_get_contents('about.md')); ?><!doctype html>
+<?php 
+	header('Content-type: text/html; Charset=UTF-8');
+	include('markdown.php');
+	$content =  Markdown(file_get_contents('about.md'));
+?><!doctype html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Документация по системе doit cms</title>
-<style>
-html
-{
-	background:#eeeeee;
+	<meta charset="UTF-8">
+	<title>Документация по системе doit cms</title>
+	<style>
+	html
+	{
+		background:#eeeeee;
 		margin-top:0;
-	padding-top:0;
-}
+		padding-top:0;
+	}
 	body
 	{
-	background:white;
-
-	padding:30px;
-	margin-top:0;
+		background:white;
+		padding:30px;
 		width:800px;
 		margin:auto;
-	
 		color: black;
 		font-family: Palatino, "Palatino Linotype", "URW Palladio L", "Book Antiqua", Georgia, serif;
 		line-height: 1.45;
 	}
-</style>
+	</style>
 </head>
 <body>
 	<?=$content ?>
