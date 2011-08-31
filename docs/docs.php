47d04e6954b176cf2abaf9cb2cf2d0032ccaa1cc
ainu <ainu.sky@gmail.com>
2011-08-31 17:18:47 +0400

    Доработка документации

--- docs/docs.php
+++ docs/docs.php
@@ -1,4 +1,8 @@
 <?php 
+$start_time = microtime();
+$start_array = explode(" ",$start_time);
+$start_time = $start_array[1] + $start_array[0];
+
 	header('Content-type: text/html; Charset=UTF-8');
 	$content = "<ul>";
 	$f = opendir('.');
@@ -66,5 +70,16 @@
 </head>
 <body>
 	<?=$content ?>
+	<?php 
+
+$end_time = microtime();
+$end_array = explode(" ",$end_time);
+$end_time = $end_array[1] + $end_array[0];
+$time = $end_time - $start_time;
+	
+
+	
+  printf("<!-- %f seconds -->",$time, );
+	?>
 </body>
 </html>
