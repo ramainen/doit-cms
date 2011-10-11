67ce0962f81dbd8945af30281ce91a7460760238
ainu <ainu.sky@gmail.com>
2011-10-11 08:25:22 +0400

    Поддержка передачи подготовленных бъектов в foreach
        <foreach d()->User->find_by_type('admin') as user>
            {user.name}
        <foreach>

--- cms/cms.php
+++ cms/cms.php
@@ -81,6 +81,7 @@ class doitClass
 		self::$instance = $this;
 		$this->template_patterns[]=	'/<foreach\s+(.*?)\s+as\s+([a-zA-Z0-9_]+)>/';
 		$this->template_replacements[]='<'.'?php $tmparr= $this->$1;
+		if(is_object($tmparr)) {$tmparr = $tmparr->all;}
 if(is_string($tmparr) || (is_array($tmparr) && (count($tmparr)!=0) && !array_key_exists(0,$tmparr))) $tmparr=array($tmparr);
 foreach($tmparr as $key=>$subval)
 	if(is_string($subval)) print $subval;else {
@@ -102,6 +103,7 @@ foreach($tmparr as $key=>$subval)
 		
 		$this->template_patterns[]='/<foreach\s+(.*?)>/';
 		$this->template_replacements[]='<'.'?php $tmparr= $this->$1;
+		if(is_object($tmparr)) {$tmparr = $tmparr->all;}
 if(is_string($tmparr) || (is_array($tmparr) && (count($tmparr)!=0) && !array_key_exists(0,$tmparr))) $tmparr=array($tmparr);
 foreach($tmparr as $key=>$subval)
 	if(is_string($subval)) print $subval;else {
