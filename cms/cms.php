051d69aeed151722805ab523615f884f393c1c1d
ramainen <ainu.sky@gmail.com>
2011-10-24 13:46:16 +0400

    Update cms/cms.php

--- cms/cms.php
+++ cms/cms.php
@@ -1,7 +1,7 @@
 <?php
 /*
 DoIt! CMS and VarVar framework
-Copyright (C) 2011 Fahrutdinov Damir (aka Ainu)
+Copyright (C) 2011 Fakhrutdinov Damir (aka Ainu)
 
 *      This program is free software; you can redistribute it and/or modify
 *      it under the terms of the GNU General Public License as published by
