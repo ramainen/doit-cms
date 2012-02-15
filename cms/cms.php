fd926dee47ceaad26a1fdb7fb3a4406daa240556
ramainen <ainu.sky@gmail.com>
2012-02-15 14:07:57 +0400

    Восстановление после экспериментов с PostgreSQL. Сама поддержка Postgre ещё не завершена, но Mysql часть стабильна.

--- cms/cms.php
+++ cms/cms.php
@@ -25,7 +25,7 @@ Copyright (C) 2011 Fakhrutdinov Damir (aka Ainu)
 	Система названа в честь статьи Variable Variables http://php.net/manual/en/language.variables.variable.php 26.01.2011
 */
 //error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
-//error_reporting(0);
+error_reporting(0);
 session_start();
 
 
--- config.php
+++ config.php
@@ -1,17 +1,17 @@
 <?php
 
-/*
+
 define('DB_TYPE','mysql');
 define('DB_HOST','127.0.0.1');
 define('DB_NAME','varvar');
 define('DB_USER','root');
 define('DB_PASSWORD','');
-*/
 
+/*
 define('DB_TYPE','pgsql');
 define('DB_HOST','localhost');
 define('DB_NAME','varvar');
 define('DB_USER','postgres');
 define('DB_PASSWORD','12345');
-
+*/
 ?>
