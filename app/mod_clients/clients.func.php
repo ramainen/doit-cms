51078b0acd6a362667cd2d43b94f5d72fcd25334
ainu <ainu.sky@gmail.com>
2012-02-24 20:04:27 +0400

    Более чистый код модуля-примера (/clients/)

--- app/mod_clients/clients.func.php
+++ app/mod_clients/clients.func.php
@@ -1,12 +1,12 @@
 <?php
-//контроллер
+
 class ClientsController 
 {
 	function index()
 	{
 		d()->clients  = d()->Client->paginate(3);
 		
-		d()->paginator = d()->Paginator->generate(d()->clients  );
+		d()->paginator = d()->Paginator->generate(d()->clients);
 
 		d()->long_paginator = d()->Paginator->generate(30);
 		print d()->view();
@@ -21,16 +21,21 @@ class ClientsController
 	function edit($id)
 	{
 		d()->client = d()->Client->find($id);
-		action('mailer');
-		action('clients#update');
 		
+		if(d()->validate('clients_update')){
+			d()->client->title=d()->params['title'];
+			d()->client->text=d()->params['text'];
+			d()->client->save;
+			header('Location: /clients/');
+			exit;
+		}
 		print d()->view();
 	}
 
 	function create()
 	{
 
-		if(d()->validate('clients#update')){
+		if(d()->validate('clients_update')){
 			d()->Client->create(array(
 				'title'=>d()->params['title'],
 				'text'=>d()->params['text']
@@ -41,24 +46,9 @@ class ClientsController
 		print d()->clients_edit_tpl();
 	}
 
-
-	function update($params)
-	{
-		d()->client->title=$params['title'];
-		d()->client->text=$params['text'];
-		d()->client->save;
-		header('Location: /clients/');
-		exit;
-	}
+ 
 	function lucky($name='')
 	{
 		print "Счастливчик $name!";
 	}
-}
-
-function mailer($params)
-{
-	print  "А вот тут отпарвка емейл апо адресу   и редирект";
-	var_dump($params);
-	exit();
 }
