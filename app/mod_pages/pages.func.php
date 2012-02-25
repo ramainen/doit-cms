09736b86a9df5bb9c1c572281746a315ef662448
ainu <ainu.sky@gmail.com>
2012-02-25 15:21:35 +0400

    Приведение примера контроллера pages в ООП-вариант.

--- app/mod_pages/pages.func.php
+++ app/mod_pages/pages.func.php
@@ -2,17 +2,17 @@
 /*
 	Модуль для работы с текстовыми страницами, для вывода меню, выода подстраниц
 */
-function pages_show()
+class PagesController
 {
-	d()->page = d()->Page->find(url(1));
-	if (d()->page->is_empty) {
-	
-		//d()->prepare_content('content',"Страница не существует".d()->add(array('pages','url'=>url(1))));
-		d()->message="Страница не существует".d()->add(array('pages','url'=>url(1)));
-		return d()->error('404');
+	function show($url)
+	{
+		d()->page = d()->Page->find_by_url($url);
+		if (d()->page->is_empty) {
+			//d()->prepare_content('content',"Страница не существует".d()->add(array('pages','url'=>url(1))));
+			d()->message="Страница не существует".d()->add(array('pages','url'=>url(1)));
+			return d()->error('404');
+		}
 		
-	} else {
 		print d()->view();
 	}
-
 }
