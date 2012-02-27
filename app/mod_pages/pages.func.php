33b206082f18033a34ac4fde64078ea9da84dcd1
ainu <ainu.sky@gmail.com>
2012-02-27 22:39:34 +0400

    Более правильный пример текстовой страницы

--- app/mod_pages/pages.func.php
+++ app/mod_pages/pages.func.php
@@ -6,8 +6,8 @@ class PagesController
 {
 	function show($url)
 	{
-		d()->page = d()->Page->find_by_url($url);
-		if (d()->page->is_empty) {
+		d()->this = d()->Page->find_by_url($url);
+		if (d()->this->is_empty) {
 			//d()->prepare_content('content',"Страница не существует".d()->add(array('pages','url'=>url(1))));
 			d()->message="Страница не существует".d()->add(array('pages','url'=>url(1)));
 			return d()->error('404');
