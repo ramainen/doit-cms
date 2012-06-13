c4af61f2629b20f46ecbe1a18c7ccf89fabb21cc
ramainen <ainu.sky@gmail.com>
2012-06-13 18:33:04 +0400

    Базовый модуль SEO

---
+++ app/mod_seo/seo.class.php
@@ -0,0 +1,54 @@
+<?php
+
+
+class Seo extends UniversalSingletoneHelper
+{
+	private $cache = array();
+	private $is_initiated = false;
+	private $seo_object = false;
+	private $seo_object_allsite = false;
+	public $title;
+	public $description;
+	public $keywords;
+	/*
+	* Обработка HTML-кода, закрытие внешних ссылок,
+	*/
+	function process($html)
+	{
+		return $html;
+	}
+
+	function init()
+	{
+		$this->title = '';
+		$this->keywords = '';
+		$this->description = '';
+		$this->text = '';
+		$this->seo_object = d()->Seoparam->where('page_url = ?',$_SERVER['REQUEST_URI'])->order_by('multi_domain DESC');
+		$this->seo_object_allsite = d()->Seoparam->where('page_url = ?','*')->order_by('multi_domain DESC');
+		if($this->seo_object->count == 0){
+			$this->seo_object = $this->seo_object_allsite;
+		}
+		
+		if($this->seo_object->title){
+			$this->title = $this->seo_object->title;
+		} else {
+			$this->title = $this->seo_object_allsite->title;
+		}
+		
+		if($this->seo_object->keywords){
+			$this->keywords = $this->seo_object->keywords;
+		} else {
+			$this->keywords = $this->seo_object_allsite->keywords;
+		}
+		
+		if($this->seo_object->description){
+			$this->description = $this->seo_object->description;
+		} else {
+			$this->description = $this->seo_object_allsite->description;
+		}
+
+		//d()->
+	}
+	
+}
--- index.php
+++ index.php
@@ -6,8 +6,12 @@
 	
 	header('Content-type: text/html; Charset=UTF-8');
 
-	$result= d()->main(); 
-
+	/*
+	//Вариант для включения модуля SEO
+	d()->Seo->init();
+	$result= d()->Seo->process(d()->main());
+	*/
+	d()->main();
 	
 	$exec_time = microtime(true) - $start_time;
 	header("X-CMS-Runtime: {$exec_time}s, ". memory_get_usage(true).'b');
