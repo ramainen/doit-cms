80d40515946b59015754f7d980e9fb0604bd8145
ramainen <ainu.sky@gmail.com>
2012-09-03 11:06:46 +0400

    rich_non_p

--- cms/mod_admin/assets/script.js
+++ cms/mod_admin/assets/script.js
@@ -52,6 +52,25 @@ $(function () {
 		theme_advanced_toolbar_align:"left",
 		theme_advanced_statusbar_location:"bottom",
 		theme_advanced_resizing:true
+	});	
+	
+	$('.tinymce_non_p').tinymce({
+		script_url:forIEscript_url,
+		language:"ru",
+		theme:"advanced",
+		skin:"o2k7",
+		convert_urls:false,
+		verify_html:false,
+		plugins:"pagebreak,style,table,save,advhr,advimage,advlink,emotions,inlinepopups,preview,media,contextmenu,paste,directionality,fullscreen,noneditable,nonbreaking,xhtmlxtras,simages,importexcel,mymodules",
+		theme_advanced_buttons1:"save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,formatselect,fontsizeselect",
+		theme_advanced_buttons2:"cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,|,undo,redo,|,link,unlink,image,simages,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+		theme_advanced_buttons3:"tablecontrols,|,removeformat,visualaid,|,sub,sup,|,charmap,emotions,media,styleprops,|,nonbreaking,pagebreak,importexcel,mymodules",
+		theme_advanced_toolbar_location:"top",
+
+		theme_advanced_toolbar_align:"left",
+		theme_advanced_statusbar_location:"bottom",
+		forced_root_block : false,
+		theme_advanced_resizing:true
 	});
 
 /*
