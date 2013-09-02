798ffb587677c1c54669bd6824d28b6536630c38
Damir Fakhrutdinov <ainu.sky@gmail.com>
2013-09-02 05:56:03 -0700

    Поддержка menu_title по умолчанию и index по умолчанию

--- app/mod_pages/page.class.php
+++ app/mod_pages/page.class.php
@@ -9,7 +9,18 @@ class Page extends ActiveRecord
 		if($this->get('link')!=''){
 			return $this->get('link');
 		}
+		if($this->get('url')=='index'){
+			return '/';
+		}
 			return '/' . $this->get('url');
+	}
+	
+	function menu_title()
+	{
+		if($this->get('menu_title')!=''){
+			return $this->get('menu_title');
+		}
+			return $this->get('title');
 	}
 }
 
