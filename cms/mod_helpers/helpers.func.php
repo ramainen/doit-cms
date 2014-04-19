f9eb60a1572c306cde2094ab5281e8616348a187
ramainen <ainu.sky@gmail.com>
2014-04-19 19:41:02 +0400

    Исправление AJAX части формы для IE8.

--- cms/mod_helpers/helpers.func.php
+++ cms/mod_helpers/helpers.func.php
@@ -217,7 +217,7 @@ function form ($params=array())
 	}
 	
 	if(isset($params['ajax']) && $params['ajax']==true) {
-		$attr .= ' onsubmit="_current_form=$(this);$.ajax({\'type\':\'post\',\'url\': $(this).attr(\'action\')?$(this).attr(\'action\'):document.location.href ,\'data\':$(this).serialize(),\'success\':function(recieved_data){eval(recieved_data)}});return false;" ';
+		$attr .= ' onsubmit="window._current_form=$(this);$.ajax({\'type\':\'post\',\'url\': $(this).attr(\'action\')?$(this).attr(\'action\'):document.location.href ,\'data\':$(this).serialize(),\'success\':function(recieved_data){eval(recieved_data)}});return false;" ';
 		
 	}
 	
