966b618ada7b38f6c8cd4300df3a4470e31d404d
Damir Fakhrutdinov <ainu.sky@gmail.com>
2015-07-03 14:00:47 +0300

    quickfix

--- app/main.func.php
+++ app/main.func.php
@@ -2,7 +2,7 @@
 function main()
 {
 
-	if(substr($_SERVER['REQUEST_URI'],-5)=='index'){
+	if(substr($_SERVER['REQUEST_URI'],-5)=='index' && !$_GET){
 		header("HTTP/1.1 301 Moved Permanently");
 		header('Location: '.substr($_SERVER['REQUEST_URI'],0,-5));
 		exit;
