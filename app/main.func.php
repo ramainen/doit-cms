75be38aee1da8d67bb779b03a63c7c0460af7d68
Damir Fakhrutdinov <ainu.sky@gmail.com>
2015-07-03 13:57:44 +0300

    Улучшение для избавления от дублей.

--- app/main.func.php
+++ app/main.func.php
@@ -1,6 +1,12 @@
 <?php
 function main()
 {
+
+	if(substr($_SERVER['REQUEST_URI'],-5)=='index'){
+		header("HTTP/1.1 301 Moved Permanently");
+		header('Location: '.substr($_SERVER['REQUEST_URI'],0,-5));
+		exit;
+	}
 	d()->content = d()->content();
 	print d()->render('main_tpl');
 }
