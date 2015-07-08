f4ad898d96bc43dfda774e615569b546fcdbe80d
damir <ainu.sky@gmail.com>
2015-07-08 12:56:44 +0300

    Новый синтаксис указания транспорта

--- cms/mod_helpers/doitmessage.class.php
+++ cms/mod_helpers/doitmessage.class.php
@@ -3,12 +3,20 @@ require_once(__DIR__. '/../vendor/Swift/lib/swift_init.php');
 //spl_autoload_register(array('Swift', 'autoload'),false,true);
 class DoitMessage extends \Swift_Message
 {
+	protected $current_transport = false;
 	
-	function send($transport=false){
+	function setTransport($transport=false){
 		if($transport===false){
 			$transport = \Swift_MailTransport::newInstance();
 		}
-		return (\Swift_Mailer::newInstance($transport)->send($this));
+		$this->current_transport = $transport;
+	}
+	
+	function send(){
+		if($this->current_transport===false){
+			$this->current_transport = \Swift_MailTransport::newInstance();
+		}
+		return (\Swift_Mailer::newInstance($this->current_transport)->send($this));
 		
 	}
  
