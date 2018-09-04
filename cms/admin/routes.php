82827855bd37efad5e0c64b4a1998d50a1b65fe7
damir <ainu.sky@gmail.com>
2018-09-04 18:09:14 +0300

    Admin 2.0: WIP: верстка панельки

--- cms/admin/routes.php
+++ cms/admin/routes.php
@@ -90,8 +90,119 @@ d()->route('/admin/var_dump', function(){
 	var_dump($_POST);
 	exit;
 });
+d()->route('/admin/edit/:url', function(){
+	
+	
+	
+});
 d()->route('/admin', function(){
 	
 	
 	
 });
+
+
+
+
+d()->route('/admin/tinymce-file-upload', function(){
+	if (isset($_FILES['image']) || isset($_FILES['file']) || !empty($_POST['base64'])) {
+		$our_folder = $_SERVER["DOCUMENT_ROOT"]."/storage";
+		$result = array();
+		if (isset($_FILES['image']) || isset($_FILES['file']) || !empty($_POST['base64'])) {
+			$is_tinymce_handler = false;
+			if(!empty($_POST['base64'])){
+				$file = '/storage/tmp_base64.png';
+				list($type, $data) = explode(';', $_POST['base64']);
+				list(, $data)      = explode(',', $data);
+				$data = base64_decode($data);
+				$ext = '.png';
+				$file = $_SERVER['DOCUMENT_ROOT'].'/storage/tmp_base64.png';
+				file_put_contents($file, $data);
+				$filename = 'blob.png';
+				$is_magic = true;
+			}elseif (isset($_FILES['image']) ){
+				$file = $_FILES['image']['tmp_name'];
+				$filename = $_FILES['image']['name'];
+				$ext = mb_strtolower(strrchr($_FILES['image']['name'],'.'));
+				$is_magic = false;
+			}else if (isset($_FILES['file'])) {
+				$is_tinymce_handler = true;
+				$file = $_FILES['file']['tmp_name'];
+				$ext = mb_strtolower(strrchr($_FILES['file']['name'],'.'));
+				$filename =  (md5_file($_FILES['file']['tmp_name'])). $ext;
+				$is_magic = false;
+			}
+			$error = false;
+			$size = false;
+			if(!in_array($ext, array('.png','.jpg','.jpeg','.gif'))){
+				$error = 'Неверное расширение файла';
+				$is_uploaded = false;
+			}else{
+				$is_uploaded =   ( !empty($_POST['base64']) || is_uploaded_file($file));
+			}
+			if (!$is_uploaded  ) {
+				$error = 'Размер файла слишком велик';
+			} else if (!$error && !($size = getimagesize($file) ) ) {
+				$error = 'Ошибка, неверный тип файла';
+			} else if (!$error && !in_array($size[2], array(1, 2, 3, 7, 8) ) ) {
+				$error = 'Ошибка типа файла, рекомендуется загружать файлы JPEG';
+			} else if (!$error && ($size[0] < 1) || ($size[1] < 1)) {
+				$error = 'Размер файла слишком маленький';
+			}
+			if ($error) {
+				$result['result'] = 'failed';
+				$result['error'] = $error;
+			} else {
+				$ext = mb_strtolower(end(explode(".", $filename)), 'UTF-8' );
+				$name = tinymce_file_upload_findname($file,$our_folder,$filename,$is_magic);
+				$source = $our_folder.'/'.$name.'.'.$ext;
+
+				if(!copy($file, $source)) {
+					$result['result'] = 'error';
+					$result['error'] = 'Ошибка при копировании файла!';
+				} else {
+					$result['result'] = 'success';
+					$newadress="/storage";
+					$newfilename= $name.'.'.$ext;
+					if(isset ($_GET['uri']))$newadress=$_GET['uri'];
+					if($newadress=="") {
+						$newadress="/storage";
+					}
+					$result['filename'] = $newadress."/".$newfilename;
+					$result['size'] = "Загружено изображение ({$size['mime']}) размером {$size[0]}px/{$size[1]}px.";
+				}
+			}
+		} else {
+			$result['result'] = 'error';
+			if($_GET['lng']=='ru') {
+				$result['error'] = 'Нет файла или внутренняя ошибка!';
+			} else {
+				$result['error'] = 'No file or an internal error!';
+			}
+		}
+		
+		$return=array();
+		foreach ($result as $key=>$val) {
+			$return[$key] =   $val ;
+		}
+		if($is_tinymce_handler ){
+			print json_encode(array('location'=>$return['filename']));
+		}else{
+			print '<script>';
+			print "window.parent.tmp_tinymce_editor.insertContent('<img src=\"". htmlspecialchars($return['filename']) ."\" alt=\"\" />');";
+			print '</script>';
+		}
+	
+	} 
+	exit;
+	
+});
+
+
+
+
+
+
+
+
+d()->route('/admin/:u*', function(){ }); //Костыль для 404
