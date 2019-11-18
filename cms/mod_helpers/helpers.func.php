3c0e52c327f3ce6f92708358b9f7e2d98720792d
Damir Fakhrutdinov <ainu.sky@gmail.com>
2019-11-18 15:55:26 +0300

    добавлен agency домен в качестве валидного в valid_email

--- cms/mod_helpers/helpers.func.php
+++ cms/mod_helpers/helpers.func.php
@@ -1113,7 +1113,7 @@ function valid_email($value,$params)
 	return ( 1 == preg_match(
 		'/^[-a-z0-9\!\#\$\%\&\'\*\+\/\=\?\^\_\`\{\|\}\~]+(?:\.[-a-z0-9!' .
 			'\#\$\%\&\'\*\+\/\=\?\^\_\`{|}~]+)*@(?:[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)*'.
-			'(?:aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$/' ,$value));
+			'(?:aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|agency|[a-z][a-z])$/' ,$value));
 }
 
 function must_be_empty($value,$params)
